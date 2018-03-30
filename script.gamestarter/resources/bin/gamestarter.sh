b68b26cb3e810451aace637a9e4f6f167dcfb3a1
RetroBlogPi <josecobo43@gmail.com>
2018-03-30 23:55:47 +0200

    5555

--- script.gamestarter/resources/bin/gamestarter.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-. /etc/profile
-
-DIR="/storage/.kodi/addons/script.gamestarter"
-
-systemd-run $DIR/resources/bin/gamestarter.start "$@" &> /storage/.kodi/userdata/addon_data/script.gamestarter/gamestarter.log
